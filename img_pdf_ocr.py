5cd6e320604739f20c6582d76164f459816b1530
Tom Peck <tapeck@umass.edu>
2017-09-09 20:50:40 -0400

    seems to be working pretty well

--- img_pdf_ocr.py
+++ img_pdf_ocr.py
@@ -17,8 +17,8 @@ for img in image_jpeg.sequence:
     img_page = Image(image=img)
     req_image.append(img_page.make_blob('jpeg'))
 
-ignore_str = ['PROPERTY', 'ID CASH']
-final_text = []
+ignore_str = ['PROPERTY', 'ID','ID CASH']
+data = [] # list of tuples (property id, cash value)
 
 # for each image of page
 for img in req_image:
@@ -30,21 +30,22 @@ for img in req_image:
             )
     for lnBx in lnBxs:
         # for each line box
-        # break down into word boxes, we only have two cols
-        # so we can try to guess which col the word is from by looking at the position
-        # 2125 seems to be just about the bottom left x position for the cash col
-        # x >= 2124 is a cash, x < 2125 is a property id
-
-    # suspectCol = filter(lambda col: col == lnBx[0].content,col_names)
-    # if suspectCol.size < 1:
-        # #couldnt find col
-        # next
-    # elif suspectCol.size > 1:
-        # # too many suspect cols we cant really classify this
-        # next
-    # # else exactly one which is great
-    #for i in range(0, len(lnBx)): final_text.append(lnBx[i].content + '\n')
+        property_id = ''
+        cash = ''
+        for Bx in lnBx.word_boxes:
+            if Bx.content in ignore_str: break # next line this is a header
+            # break down into word boxes, we only have two cols
+            # so we can try to guess which col the word is from by looking at the position
+            # 2125 seems to be just about the bottom left x position for the cash col
+            # x >= 2100 is a cash, x < 2100 is a property id
+            if Bx.position[0][0] >= 2100: # position lower left x
+                cash += Bx.content
+            else: # x < 2100
+                property_id += Bx.content
+        if property_id and cash: # only add line if we have something
+            data.append((property_id,cash)) # append final find to data
 
 f = open('output_file', 'w')
-for s in final_text:
-    f.write(s.encode('UTF-8'))
+f.write('property_id,cash\n')
+for data_pair in data:
+    f.write(data_pair[0].encode('UTF-8') + ',' + data_pair[1].encode('UTF-8') + '\n')
