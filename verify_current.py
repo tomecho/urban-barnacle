c8771783f49888bfe8c2979488cafe619cee2fde
Tom Peck <tom@tompeck.me>
2018-08-21 23:09:42 -0400

    select search result rows and start mapping them to result dict

--- verify_current.py
+++ verify_current.py
@@ -1,14 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import sys
+
+def visit_site(web):
+    web.get('http://ctbiglist.com')
+    web.find_element_by_id('PropertyID').send_keys(sys.argv[1])
+    web.find_element_by_xpath("//input[@name='Submit']").click()
+
+    search_result_rows = web.find_elements_by_xpath("(//form[@action='UP_Claim.asp']//tbody)[1]/tr[position() > 1]")
+    map(
+        lambda row: {
+            
+        }, 
+        search_result_rows
+    )
 
 options = Options()
 options.set_headless(headless=True)
 web = webdriver.Firefox(firefox_options=options)
-try:
-    visit_google(web)
-except(ex):
-    print ex.message,
-
-def visit_google(web):
-    web.get('http://google.com')
-
+visit_site(web)
+web.quit()
