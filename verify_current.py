c018cc61da1a173cab43da4d678dd6a7ce6bcf38
Tom Peck <tom@tompeck.me>
2018-08-23 22:02:38 -0400

    add create table and write sqlite functions

--- verify_current.py
+++ verify_current.py
@@ -1,24 +1,38 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import sqlite3
 import sys
 
+def create_table(property_table_name):
+    tbl_name = '[{property_table_name} withpeople]'.format(property_table_name=property_table_name)
+    with sqlite3.connect('output.db') as conn:
+        conn.execute('CREATE TABLE '+ tbl_name +' (property_id int primary key, cash real, source_location text, name text, address_street text, address_city text, address_state text, address_zip text);')
+        conn.commit()
+    return tbl_name
+
+def write_sqlite(result_array):
+    with sqlite3.connect('output.db') as conn:
+        conn.execute('INSERT INTO ' + tbl_name + ' VALUES(?,?,?,?,?,?,?,?);', result_array)
+        conn.commit()
+
 def translate_properties(web, properties):
-    web.get('http://ctbiglist.com')
-    for propertyId in properties:
+    for _property in properties:
         web.find_element_by_id('PropertyID').send_keys(propertyId)
         web.find_element_by_xpath("//input[@name='Submit']").click()
 
         search_result_rows = web.find_elements_by_xpath("(//form[@action='UP_Claim.asp']//tbody)[1]/tr[position() > 1]")
-        result_dict = map(
+        result_array = map(
             lambda row:
-            {
-                'property_id': propertyId,
-                'name': row.find_element_by_xpath('td[3]').text,
-                'address_street': row.find_element_by_xpath('td[4]').text,
-                'address_city': row.find_element_by_xpath('td[5]').text,
-                'address_state': row.find_element_by_xpath('td[6]').text,
-                'address_zip': row.find_element_by_xpath('td[7]').text,
-            }, 
+            [
+                _property['id'],
+                _property['cash'],
+                _property['source_location'],
+                row.find_element_by_xpath('td[3]').text,
+                row.find_element_by_xpath('td[4]').text,
+                row.find_element_by_xpath('td[5]').text,
+                row.find_element_by_xpath('td[6]').text,
+                row.find_element_by_xpath('td[7]').text,
+            ], 
             search_result_rows
         )
 
@@ -27,4 +41,5 @@ propertyId = sys.argv[1]
 options = Options()
 options.set_headless(headless=True)
 with webdriver.Firefox(firefox_options=options) as web:
+    web.get('http://ctbiglist.com')
     translate_properties(web, [propertyId])
