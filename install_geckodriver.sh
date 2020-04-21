881f86966aa4d4c08f667efc4d0f63128a92b388
Tom Peck <tom@tompeck.me>
2020-04-20 22:46:08 -0400

    install firefox too

--- install_geckodriver.sh
+++ install_geckodriver.sh
@@ -2,4 +2,5 @@ wget https://github.com/mozilla/geckodriver/releases/download/v0.21.0/geckodrive
 sh -c 'tar -x geckodriver -zf geckodriver-v0.21.0-linux64.tar.gz -O > /usr/bin/geckodriver'
 chmod +x /usr/bin/geckodriver
 rm geckodriver-v0.21.0-linux64.tar.gz
+apt update && apt install -y firefox
 
